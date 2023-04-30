5c15786fd4414a44bb5550f40a1364453a6cdb7d
kratos2377 <shobityadav23@gmail.com>
2023-04-30 18:11:29 +0530

    Instructions added in lib.rs

--- programs/cohesion/src/lib.rs
+++ programs/cohesion/src/lib.rs
@@ -28,5 +28,79 @@ pub mod cohesion {
 	pub fn delete_tweet(ctx: Context<DeleteTweet>) -> Result<()> {
 		instructions::delete_tweet(ctx)
 	}
+
+	// Voting Instructions
+	pub fn vote(ctx: Context<Vote>, tweet: Pubkey, result: VotingResult, voting_bump: u8) -> Result<()> {
+		instructions::vote(ctx, tweet, result, voting_bump)
+	}
+
+	pub fn update_voting(ctx: Context<UpdateVoting>, new_result: VotingResult) -> Result<()> {
+		instructions::update_voting(ctx, new_result)
+	}
+    
+	// Reaction Instructions
+	pub fn react(ctx: Context<React>, tweet: Pubkey, reaction_char: String, reaction_bump: u8) -> Result<()> {
+		instructions::react(ctx, tweet, reaction_char, reaction_bump)
+	}
+
+	pub fn update_reaction(ctx: Context<UpdateReaction>, new_reaction_char: String) -> Result<()> {
+		instructions::update_reaction(ctx, new_reaction_char)
+	}
+
+	pub fn delete_reaction(_ctx: Context<DeleteReaction>) -> Result<()> {
+		instructions::delete_reaction(_ctx)
+	}
+
+	// Comments Instructions
+	pub fn send_comment(ctx: Context<SendComment>, tweet: Pubkey, content: String, parent: Option<Pubkey>) -> Result<()> {
+		instructions::send_comment(ctx, tweet, content, parent)
+	}
+	
+	pub fn update_comment(ctx: Context<UpdateComment>, new_content: String) -> Result<()> {
+		instructions::update_comment(ctx, new_content)
+	}
+	
+	pub fn delete_comment(ctx: Context<DeleteComment>) -> Result<()> {
+		instructions::delete_comment(ctx)
+	}
+	
+	// DM Instrcutions
+	pub fn send_dm(ctx: Context<SendDm>, recipient: Pubkey, content: String) -> Result<()> {
+		instructions::send_dm(ctx, recipient, content)
+	}
+
+	pub fn update_dm(ctx: Context<UpdateDm>, new_content: String) -> Result<()> {
+		instructions::update_dm(ctx, new_content)
+	}
+
+	pub fn delete_dm(_ctx: Context<DeleteDm>) -> Result<()> {
+		instructions::delete_dm(_ctx)
+	}
+
+	// User-Alias Instructions 
+	pub fn create_user_alias(ctx: Context<CreateUserAlias>, alias: String) -> Result<()> {
+		instructions::create_user_alias(ctx, alias)
+	}
+
+	pub fn update_user_alias(ctx: Context<UpdateUserAlias>, new_alias: String) -> Result<()> {
+		instructions::update_user_alias(ctx, new_alias)
+	}
+
+	pub fn delete_user_alias(_ctx: Context<DeleteUserAlias>) -> Result<()> {
+		instructions::delete_user_alias(_ctx)
+	}
+    
+	// Status Instructions
+	pub fn create_status(ctx: Context<CreateStatus>, alias: String) -> Result<()> {
+		instructions::create_status(ctx, alias)
+	}
+
+	pub fn update_status(ctx: Context<UpdateStatus>, new_alias: String) -> Result<()> {
+		instructions::update_status(ctx, new_alias)
+	}
+
+	pub fn delete_status(_ctx: Context<DeleteStatus>) -> Result<()> {
+		instructions::delete_status(_ctx)
+	}
 }
 
