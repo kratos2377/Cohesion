303484a8753c52b579ac1ce85a2bee62d47e622e
kratos2377 <shobityadav23@gmail.com>
2023-04-30 18:16:34 +0530

    Configured mod.rs file

--- programs/cohesion/src/state/mod.rs
+++ programs/cohesion/src/state/mod.rs
@@ -1,5 +1,15 @@
 pub use tweet::*;
+pub use dm::*;
+pub use status::*;
+pub use voting::*;
+pub use reaction::*;
+pub use comment::*;
+pub use user_alias::*;
 
-
-
+pub mod comment;
+pub mod reaction;
+pub mod status;
 pub mod tweet;
+pub mod user_alias;
+pub mod voting;
+pub mod dm;
