a4d12e38fa0c246c6c520c05457362e5a778a32b
kratos2377 <shobityadav23@gmail.com>
2023-05-01 13:47:16 +0530

    Wallets added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import ConnectWallet from './components/connectWallet';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+
 
 function App() {
+  const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+]
+
   return (
-    <div className="App">
-    <header className="App-header">
-      <h1>Solana Examples</h1>
-      <hr className="fullWidth" />
+    <div className="w-full max-w-3xl lg:max-w-4xl mx-auto">
 
-      <p>Hello there</p>
-      <ConnectWallet/>
 
-    </header>
-  </div>
+    <div className="py-4 md:py-8 md:pl-4 md:pr-8 fixed w-20 md:w-64">
+      Side bar will come here
+    </div>
+
+
+    <main className="flex-1 border-r border-l ml-20 md:ml-64 min-h-screen">
+        <header className="flex space-x-6 items-center justify-between px-8 py-4 border-b">
+            <div className="text-xl font-bold"></div>
+        </header>
+       
+    </main>
+</div>
   );
 }
 
