071f9d0ab7739d0c453dd393706ac8c545387c37
kratos2377 <shobityadav23@gmail.com>
2023-05-02 11:14:29 +0530

    Sidebar added and login page fixed

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,29 +1,34 @@
 import { useWallet } from '@solana/wallet-adapter-react'
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import { Payment } from './Payment'
 import Image from 'next/image'
+import HomePage from '@/pages/homepage'
+import { useEffect, useState } from 'react'
 const Login = () => {
   const wallet = useWallet()
+  const [loadPage  , setLoadPage] = useState(false);
 
-  /** show payment UI if wallet is connected */
-  if (wallet.connected) return <Payment />
+    useEffect(() => {
+      if(wallet.connected) {
+        setLoadPage(true)
+      }
+    } , [wallet]) 
 
   return (
-    <div className={styles.loginPage}>
-         <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="m-10 relative light:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-        <div className={styles.description}>
-        Cohesion is a social platform build on solana blockchain.
-        </div>
-      <p className={styles.text}>Login to access this app</p>
-      <WalletMultiButton />
-    </div>
+    ( loadPage ? <HomePage />  : <div className={styles.loginPage}>
+    <Image
+         src="/vercel.svg"
+         alt="Vercel Logo"
+         className="m-10 relative light:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+         width={100}
+         height={24}
+         priority
+       />
+   <div className={styles.description}>
+   Cohesion is a social platform build on solana blockchain.
+   </div>
+ <p className={styles.text}>Login to access this app</p>
+ <WalletMultiButton />
+</div>)
   )
 }
 
